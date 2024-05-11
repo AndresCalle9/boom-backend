e9d331eb767cb5e321a433a7c981642447713471
Andrés Calle <andrescalleuribe@gmail.com>
2024-05-11 08:27:44 -0500

    add qty to model

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -1,27 +1,29 @@
 import mongoose from 'mongoose';
 
 export interface Product {
-    id: number;
-    name: object;
-    imageUrl: string;
-    category: string;
-    price: number;
-    unit: string;
-    enable: boolean;
-    createdAt: Date;
-    updatedAt: Date;
-  }
+  id: number;
+  name: object;
+  imageUrl: string;
+  category: string;
+  price: number;
+  unit: string;
+  enable: boolean;
+  qty: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
-  const ProductsSchema = new mongoose.Schema<Product>({
-    name: {type: Object, required: true},
-    id: {type: Number},
-    imageUrl: {type: String, required: true},
-    category: {type: String, required: true},
-    price: {type: Number, required: true},
-    unit: {type: String, required: true},
-    enable: {type: Boolean},
-    createdAt: {type:Date},
-    updatedAt:{type:Date},
-  });
-  
+const ProductsSchema = new mongoose.Schema<Product>({
+  name: { type: Object, required: true },
+  id: { type: Number },
+  imageUrl: { type: String, required: true },
+  category: { type: String, required: true },
+  price: { type: Number, required: true },
+  unit: { type: String, required: true },
+  enable: { type: Boolean },
+  qty: { type: Number },
+  createdAt: { type: Date },
+  updatedAt: { type: Date },
+});
+
 export const ProductModel = mongoose.model('Products', ProductsSchema);
